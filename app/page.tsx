24db0247ac2c5e5ba56326145ddc303f7c83b37d
Oleksandr Koval <timew2762@gmail.com>
2024-07-27 09:27:55 +0300

    add categories and top bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Container, Title, TopBar } from '@/components/common'
+
 export default function Home() {
-	return <></>
+	return (
+		<>
+			<Container className='mt-10'>
+				<Title text='All pizzas' size='lg' className='font-extrabold' />
+			</Container>
+			<TopBar />
+		</>
+	)
 }
